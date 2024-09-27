2eafe0c1d9aa3d794cb31b564ca7075902a0fe42
fahad111000 <fahad.kh2210@gmail.com>
2024-09-28 02:37:50 +0500

    Adding functionality in navbar

---
+++ script.js
@@ -0,0 +1,8 @@
+console.log("Works!")
+
+var unorderList = document.getElementById('unorder');
+
+function navIcon() {
+    console.log("Button Works!");
+    unorderList.classList.toggle('ul-active')
+}
