faf955de8719946bf1bfb3d5262d79882e55ab4f
fahad111000 <fahad.kh2210@gmail.com>
2024-09-28 02:44:54 +0500

    Adding functionality in navbar (humburger icons)

--- script.js
+++ script.js
@@ -1,8 +1,10 @@
 console.log("Works!")
 
 var unorderList = document.getElementById('unorder');
+var humberger = document.getElementById('bars');
 
 function navIcon() {
     console.log("Button Works!");
     unorderList.classList.toggle('ul-active')
+    humberger.classList.toggle('fa-xmark')
 }
