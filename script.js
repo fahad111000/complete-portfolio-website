00fc0b60915aadfa8958b62d029b2191520dd230
fahad111000 <fahad.kh2210@gmail.com>
2025-04-07 02:26:03 +0500

    added functionality in html/css navbar

--- script.js
+++ script.js
@@ -1,8 +1,62 @@
 var unorderList = document.getElementById('unorder');
 var humberger = document.getElementById('bars');
 
+let htmlCssButton = document.getElementById('htmlCssProjects');
+let jsButton = document.getElementById('jsProjects');
+let singelProject = document.querySelectorAll('.singel-project');
+let allProjects = document.getElementById('allProjects');
+
+allProjects.addEventListener('click', (e) => {
+    e.preventDefault();
+    singelProject.forEach((project) => {
+        project.style.display = 'block'
+    });
+
+})
+
+
+htmlCssButton.addEventListener('click', (e) => {
+    e.preventDefault();
+    singelProject.forEach((project) => {
+        if (project.classList.contains("html")) {
+            project.style.display = 'block'
+        }
+
+        else {
+            project.style.display = "none";
+
+        }
+    })
+    // jsButton.classList.add("disabled");
+
+})
+
+jsButton.addEventListener('click', (e) => {
+    e.preventDefault();
+    singelProject.forEach((project) => {
+        if (project.classList.contains("js")) {
+            console.log("wor")
+            project.style.display = 'block'
+        }
+
+        else {
+            project.style.display = "none";
+
+        }
+    })
+    // htmlBtn.classList.add("disabled");
+})
+
+
+
+
 function navIcon() {
     console.log("Button Works!");
     unorderList.classList.toggle('ul-active')
     humberger.classList.toggle('fa-xmark')
 }
+
+
+//
+
+// ___________GSAP animation_________________
